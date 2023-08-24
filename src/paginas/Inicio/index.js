9beffc7f40a383e9e67b4fb5a3d3aa654c77f562
Matheus Pereira <matheus-p-p@hotmail.com>
2023-08-23 22:24:30 -0300

    Rotas aninhadas na Routes e página sobre mim

--- src/paginas/Inicio/index.js
+++ src/paginas/Inicio/index.js
@@ -1,20 +1,15 @@
-import Banner from "componentes/Banner";
 import Post from "componentes/Post";
 import styles from './Inicio.module.css';
 import posts from 'json/posts.json';
 
 export default function Inicio() {
     return (
-        <main>
-            <Banner />
-
-            <ul className={styles.posts}>
-                {posts.map((post) => (
-                    <li key={post.id}>
-                        <Post post={post} />
-                    </li>
-                ))}
-            </ul>
-        </main>
+        <ul className={styles.posts}>
+            {posts.map((post) => (
+                <li key={post.id}>
+                    <Post post={post} />
+                </li>
+            ))}
+        </ul>
     )
 }
